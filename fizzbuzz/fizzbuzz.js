96f2e586262fab1e6b712671cc89d0228501071d
康平大栗 <koheiokuri@Kohei-MacBook-Pro.local>
2025-11-22 19:02:13 +0900

    PRtestt

--- fizzbuzz/fizzbuzz.js
+++ fizzbuzz/fizzbuzz.js
@@ -1,8 +1,9 @@
+//定数化
 
 const n = 1000;
-const fizzbuzzN = 15;
-const fizzN = 3;
-const buzzN = 5;
+const fizzbuzzN = 15; //fizzbuzzの時の数字
+const fizzN = 3; //fizzの時
+const buzzN = 5; //buzzの時
 
 for(let i=1; i<=n; i++){
     if(i % fizzbuzzN === 0){
